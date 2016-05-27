2715c65023f0a14d90413a3ecdb635e29254d012
michbur <michalburdukiewicz@gmail.com>
2016-05-27 13:01:27 +0200

    added references and started case study

--- dpcR_poster_functions.R
+++ dpcR_poster_functions.R
@@ -2,7 +2,8 @@ source("my_ggplot_theme.R")
 
 library(dplyr)
 library(reshape2)
-
+library(qpcR)
+library(dpcR)
 
 qpcr_dat <- reps2[, c(1L:15)] %>% 
   mutate(Cycles = factor(Cycles)) %>% 
@@ -17,23 +18,30 @@ p1 <- ggplotGrob(ggplot(qpcr_dat, aes(x = Cycles, y = Fluorescence, color = vari
   geom_line() +
   scale_color_discrete(guide = FALSE) +
   scale_x_continuous("Cycle") +
-  my_theme)
+  my_theme +
+  ggtitle("qPCR data"))
 
 p2 <- ggplotGrob(ggplot(qpcr2pp_dat, aes(x = Cycles, y = lambda)) +
   geom_step() +
   geom_rug(sides="b") +
   scale_y_continuous(expression(lambda)) +
   scale_x_continuous("Cycle", limits = c(1, 45)) +
-  my_theme)
+  my_theme+
+  ggtitle("qPCR data in dPCR representation"))
 
 max_width = unit.pmax(p1[["widths"]][2:5], p2[["widths"]][2:5])
 p1[["widths"]][2:5] <- as.list(max_width)
 p2[["widths"]][2:5] <- as.list(max_width)
 
-cairo_ps("./dpcr_figures/qpcr2pp.eps", width = 13, height = 8, onefile = FALSE)
-grid.draw(grobTree(rectGrob(gp=gpar(fill=adjustcolor("white", alpha.f = 0))), 
-                   grid.arrange(textGrob("A", x = 0.75, y = 0.9, gp=gpar(fontsize=24)), p1, 
-                                textGrob("B", x = 0.75, y = 0.9, gp=gpar(fontsize=24)), p2,
-                                nrow = 2, ncol = 2, widths = c(0.05, 0.95))))
+cairo_ps("./dpcR_figures/qpcr2pp.eps", width = 13, height = 8)
+
+empty_grob <- rectGrob(gp=gpar(fill = "transparent", col = "transparent"))
+
+cairo_ps("./dpcR_figures/qpcr2pp.eps", width = 13, height = 8)                  
+qpcr2pp_plot <- grid.arrange(textGrob("A", x = 0.75, y = 0.9, gp=gpar(fontsize=24)), p1, empty_grob,
+                             textGrob("B", x = 0.75, y = 0.9, gp=gpar(fontsize=24)), p2, empty_grob,
+                             nrow = 2, ncol = 3, widths = c(0.05, 0.9, 0.05))
+grid.newpage()
+grid.draw(qpcr2pp_plot)
 dev.off()
 
